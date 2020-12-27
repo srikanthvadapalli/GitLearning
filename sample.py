6f2f531b2e1b86dec11ebf0f0219602d28804bfc
srikanthvadapalli <k.pranuthi@gmail.com>
2020-12-27 11:03:05 -0500

    removed a line & added a new line at the bottom

--- sample.py
+++ sample.py
@@ -1,5 +1,4 @@
 print("My name is Srikanth")
 print("My age is 32")
-print("I am working for TCS")
 print("I want to be an ethical hacker")
-
+print("I want to join Google as EH/Cyber Security Engineer")
